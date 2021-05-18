bfeffaf815d9324277b1144099ad3691548a4823
xtong019 <81706195+xtong019@users.noreply.github.com>
2021-05-18 12:43:12 -0700

    Update item.hpp

--- item.hpp
+++ item.hpp
@@ -19,12 +19,12 @@ private:
     vector <Character*> ShopList;
 public:
     ~Item();
-    Item(std::string name, int effect, int type, int profession, int price): name(name), effect(effect), type(type), profession(profession), price(price) {}
+    Item(std::string name, int effect, int type, int profession, int price): name(name), effect(effect), type(type), profession(profession), price(price) {};
     string getItem();
     int gettype();
     int geteffect();
     int getprofession();
     int getprice();
     void print_list(vector List);
-}
+};
 #endif
