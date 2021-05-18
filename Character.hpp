0beda2867c7427ef5fc161b1d53b5854d52a7a70
Jiaxing Lin <81549188+jlin1110@users.noreply.github.com>
2021-05-18 01:23:27 -0700

    Create Character.hpp

---
+++ Character.hpp
@@ -0,0 +1,29 @@
+#ifndef __CHARACTER_HPP__
+#define __CHARACTER_HPP__
+#include <string>
+
+class Character
+{
+  public:
+	Character(string n, double hp, double power, double armor);
+	~Character();
+
+	double getHealthPoint();
+	double getPower();
+	double getArmor();
+	string getName();
+	
+
+	void setHealthPoint(double hp);
+	void setPower(double p);
+	void setArmor(double a);
+	
+	virtual int getCoins() = 0;
+
+private:
+	double healthPoint;
+	double power;
+	double armor;
+	string name;
+	int coins;
+}
