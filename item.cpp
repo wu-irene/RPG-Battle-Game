ed6cea8f33f265b320ca6ba57ae6a21d4c3df45c
xtong019 <81706195+xtong019@users.noreply.github.com>
2021-05-18 11:00:27 -0700

    Update item.cpp

--- item.cpp
+++ item.cpp
@@ -1,12 +1,18 @@
 include "item.hpp"
   
-string getItem(){return name;}
-    int gettype() { return type; }
-    int geteffect() { return effect; }
-    int getprofession() { return profession; }
-    int getprice() { return price; }
-    void print_list(vector List){
+  Item::~Item()
+  {
+    delete itemList;
+  }
+
+  string Item::getItem(){return name;}
+  int Item::gettype() { return type; }
+  int Item::geteffect() { return effect; }  
+  int Item::getprofession() { return profession; }
+  int Item::getprice() { return price; }
+  void Item::print_list(vector List){
         for( int i = 0; i < List.size(); i++)
     {
       std::cout << (List.at(i)).(name)<<endl;
     }
+  }
