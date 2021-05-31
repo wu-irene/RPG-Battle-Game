b2c826d185f4a7cdf63c1e7032bd94487be87697
Jiaxing Lin <81549188+jlin1110@users.noreply.github.com>
2021-05-31 01:08:58 -0700

    Create ILevelableCharacter.hpp

---
+++ ILevelableCharacter.hpp
@@ -0,0 +1,13 @@
+#ifndef __ILEVELABLECHARACTER_HPP__
+#define __ILEVELABLECHARACTER_HPP__
+using namespace std;
+
+class ILevelableCharacter
+{
+public:
+    ~ILevelableCharacter() {}
+
+    virtual void AddCurrentExp(double exp) = 0;
+};
+
+#endif
