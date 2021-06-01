75a87e8735fceca8d9628de71399d98a37265421
Jiaxing Lin <81549188+jlin1110@users.noreply.github.com>
2021-06-01 08:26:15 -0700

    Update MonsterWithInventory.hpp

--- MonsterWithInventory.hpp
+++ MonsterWithInventory.hpp
@@ -1,22 +1,27 @@
 #ifndef __MONSTERWITHINVENTORY_HPP__
 #define __MOSNTERWITHINVENTORY_HPP__
+#include "IBattalableCharacter.h"
+#include "IEquipableCharacter.h"
+#include "EquipmentBackpack.h"
+#include "DroppableBackpack.h"
+#include "Equipment.h"
 #include <string>
 using namespace std;
 
 template <class TCharacter>
-class MonsterWithInventory : public IBattlableCharacter, IEquipableCharacter
+class MonsterWithInventory : public IBattalableCharacter, IEquipableCharacter
 {
 private:
 	TCharacter _character;
 	EquipmentBackpack _equipmentBackpack;
 	double _currentHp;
 public:
-	MonsterWithInventory();
-	bool AddItem(Equipment equipment);
-	bool RemoveItem(Equipment equipment);
-	bool Equip(Equipment equipment);
-	bool UnEquip(Equipment equipment);
-	bool ToggleEquipStatus(Equipment equipment);
+	MonsterWithInventory(TCharacter character, EquipmentBackpack equipmentBackpack);
+	bool AddItem(Equipment* equipment);
+	bool RemoveItem(Equipment* equipment);
+	bool Equip(Equipment* equipment);
+	bool UnEquip(Equipment* equipment);
+	bool ToggleEquipStatus(Equipment* equipment);
 	int GetEquipmentCount();
 	int GetEquippedEquipmentCount();
 	int GetUnEquippedEquipmentCount();
