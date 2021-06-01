bb917f12dc8cf9c40f03f59be1e01e8d384c0dee
xtong019 <81706195+xtong019@users.noreply.github.com>
2021-06-01 09:08:09 -0700

    Add files
    add bloodthorn

---
+++ Bloodthorn.cpp
@@ -0,0 +1,21 @@
+#include "Bloodthorn.h"
+
+double Bloodthorn::GetHp()
+{
+    return 10;
+}
+
+double Bloodthorn::GetPower()
+{
+    return 50;
+}
+
+double Bloodthorn::GetArmor()
+{
+    return 2;
+}
+
+double Bloodthorn::GetPrice()
+{
+    return 300;
+}
